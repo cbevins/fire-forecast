c7966a1c29f1a8e334858862a3bd4046edcb3d1c
Collin D. Bevins <cbevins@montana.com>
2021-05-27 14:45:52 -0600

    Added leaner fuel model key

--- src/models/FuelModelKey.js
+++ src/models/FuelModelKey.js
@@ -252,3 +252,228 @@ export const FuelModelKey = [{
     desc: 'NB9 Bare ground.'
   }]
 }]
+
+export const Grass = {
+  label: 'Grass',
+  options: [{
+    label: 'Arid to semi-arid climate, 15% extinction moisture',
+    options: [{
+      label: 'Grass is short, patchy, and possibly heavily grazed.',
+      fuel: 'gr1'
+    }, {
+      label: 'Moderately coarse continuous grass, average depth about 1 foot.',
+      fuel: 'gr2'
+    }, {
+      label: 'Moderately coarse continuous grass, average depth about 2 feet.',
+      fuel: 'gr4'
+    }, {
+      label: 'Moderately coarse continuous grass, average depth about 3 feet.',
+      fuel: 'gr7'
+    }]
+  }, {
+    label: 'Subhumid to humid climate, 30-40% extinction moisture',
+    options: [{
+      label: 'Grass is short, patchy, and possibly heavily grazed.',
+      fuel: 'gr1',
+    }, {
+      label: 'Very coarse grass, average depth about 2 feet.',
+      fuel: 'gr3'
+    }, {
+      label: 'Dense, coarse grass, average depth about 1 to 2 feet.',
+      fuel: 'gr5'
+    }, {
+      label: 'Dryland grass about 1 to 2 feet tall.',
+      fuel: 'gr6'
+    }, {
+      label: 'Heavy, coarse, continuous grass 3 to 5 feet tall.',
+      fuel: 'gr8'
+    }, {
+      label: 'Very heavy, coarse, continuous grass 5 to 8 feet tall.',
+      fuel: 'gr9'
+    }]
+  }]
+}
+
+export const GrassShrub = {
+  label: 'Grass-Shrub',
+  options: [{
+    label: 'Arid to semi-arid climate, 15% extinction moisture',
+    options: [{
+      label: 'Shrubs are about 1 foot high, low grass load.',
+      fuel: 'gs1',
+    }, {
+      label: 'Shrubs are 1 to 3 feet high, moderate grass load.',
+      fuel: 'gs2'
+    }]
+  }, {
+    label: 'Subhumid to humid climate, 30-40% extinction moisture',
+    options: [{
+      label: 'Moderate grass/shrub load, average grass/shrub depth less than 2 feet.',
+      fuel: 'gs3'
+    }, {
+      label: 'Heavy grass/shrub load, depth greater than 2 feet.',
+      fuel: 'gs4'
+    }]
+  }]
+}
+
+export const Shrub = {
+    label: 'Shrub',
+    options: [{
+      label: 'Arid to semi-arid climate, 15% extinction moisture',
+      options: [{
+        label: 'Low shrub fuel load, fuelbed depth about 1 foot; some grass may be present.',
+        fuels: 'sh1'
+      }, {
+        label: 'Moderate fuel load (higher than SH1), depth about 1 foot, no grass fuel present.',
+        fuels: 'sh2'
+      }, {
+        label: 'Heavy shrub load, depth 4 to 6 feet.',
+        fuel: 'sh5'
+      }, {
+        label: 'SH7 Very heavy shrub load, depth 4 to 6 feet.',
+        fuel: 'sh7'
+      }]
+    }, {
+      label: 'Subhumid to humid climate, 30-40% extinction moisture',
+      options: [{
+        label: 'Moderate shrub load, possibly with pine overstory or herbaceous fuel, fuel bed depth 2 to 3 feet.',
+        fuel: 'sh3'
+      }, {
+        label: 'Low to moderate shrub and litter load, possibly with pine overstory, fuel bed depth about 3 feet.',
+        fuel: 'sh4'
+      }, {
+        label: 'Dense shrubs, little or no herb fuel, depth about 2 feet.',
+        fuel: 'sh6'
+      }, {
+        label: 'Dense shrubs, little or no herb fuel, depth about 3 feet.',
+        fuel: 'sh8'
+      }, {
+        label: 'Dense, finely branched shrubs with significant fine dead fuel, about 4 to 6 feet tall; some herbaceous fuel may be present',
+        fuel: 'sh9'
+      }]
+    }]
+}
+
+export const TimberUnderstory = {
+  label: 'Timber-Understory',
+  options: [{
+    label: 'Arid to subhumid, 20% extinction moisture',
+    options: [{
+      label: 'Fuelbed is low load of grass and/or shrub with litter.',
+      fuel: 'tu1'
+    }, {
+      label: 'Fuelbed is short conifer trees with grass or moss understory.',
+      fuel: 'tu4'
+    }, {
+      label: 'Fuelbed is high load conifer litter with shrub understory.',
+      fuel: 'tu5'
+    }]
+  }, {
+    label: 'Humid climate, 30% extinction moisture',
+    options: [{
+      label: 'Fuelbed is moderate litter load with shrub component.',
+      fuel: 'tu2'
+    }, {
+      label: 'Fuelbed is moderate litter load with grass and shrub components.',
+      fuel: 'tu3'
+    }]
+  }]
+}
+
+export const TimberLitter = {
+  label: 'Timber-Litter',
+  options: [{
+    label: 'Recently burned, able to carry fire',
+    options: [{
+      label: 'Light to moderate load, fuels 1 to 2 inches deep',
+      fuel: 'tl1',
+    }]
+  }, {
+    label: 'Not recently burned',
+    options: [{
+      label: 'Broadleaf (hardwood) litter',
+      options: [{
+        label: 'Low load, compact.',
+        fuel: 'tl2'
+      }, {
+        label: 'Moderate load, less compact.',
+        fuel: 'tl6'
+      }, {
+        label: 'Very high load, fluffy.',
+        fuel: 'tl9'
+      }]
+    }, {
+      label: 'Long-needle pine litter',
+      options: [{
+        label: 'moderate load and compactness may include small amount of herbaceous load.',
+        fuel: 'tl8',
+      }]
+    }, {
+      label: 'Other',
+      options: [{
+        label: 'Both fine and coarse fuels',
+        options: [{
+          label: 'Moderate load, includes small diameter downed logs',
+          fuel: 'tl4'
+        }, {
+          label: 'Heavy load, includes larger diameter downed logs',
+          fuel: 'tl7'
+        }]
+      }, {
+        label: 'No coarse fuels',
+        options: [{
+          label: 'Moderate load conifer litter',
+          fuel: 'tl3'
+        }, {
+          label: 'High load conifer litter; light slash or mortality fuel.',
+          fuel: 'tl5'
+        }, {
+          label: 'Very high load broadleaf litter; heavy needle-drape in otherwise sparse shrub layer',
+          fuel: 'tl9'
+        }]
+      }]
+    }]
+  }]
+}
+
+export const SlashBlowdown = {
+  label: 'Slash-Blowdown',
+  options: [{
+    label: 'Activity fuel',
+    options: [{
+      label: '1 Fine fuel load is 10 to 20 tons/acre, weighted toward fuels 1 to 3 inches diameter class, depth is less than 1 foot',
+      fuel: 'sb1'
+    }, {
+      label: 'Fine fuel load is 7 to 12 tons/acre, evenly distributed across 0 to 0.25, 0.25 to 1, and 1 to 3 inch diameter classes, depth is about 1 foot.',
+      fuel: 'sb2'
+    }, {
+      label: 'Fine fuel load is 7 to 12 tons/acre, weighted toward 0 to 0.25 inch diameter class, depth is more than 1 foot.',
+      fuel: 'sb3'
+    }],
+  }, {
+    label: 'Blowdown',
+    options: [{
+      label: 'Blowdown is scattered, with many trees still standing.',
+      fuel: 'sb2'
+    }, {
+      label: 'Blowdown is moderate, trees compacted to near the ground.',
+      fuel: 'sb3'
+    }, {
+      label: 'Blowdown is total, fuelbed not compacted, foliage still attached.',
+      fuel: 'sb4'
+    }]
+  }]
+}
+
+export const ShortKey = {
+  label: 'The fire-carrying fuel type is:',
+  options: [
+    Grass,
+    GrassShrub,
+    Shrub,
+    TimberUnderstory,
+    TimberLitter,
+    SlashBlowdown
+  ]
+}
